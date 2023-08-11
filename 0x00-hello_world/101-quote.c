4047e0b124bf7a226ca214d5388fefcbbe3a3455
Yara-ib <yr.ap.23@gmail.com>
2023-08-10 19:59:28 -0700

    Update

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+if (x=int)
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+else
+	return (0);
+}
+
