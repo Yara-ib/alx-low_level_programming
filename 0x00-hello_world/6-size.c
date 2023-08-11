5ac46a731d45bfc056e7b8d397950c7c94ae283c
Yara-ib <yr.ap.23@gmail.com>
2023-08-10 19:47:34 -0700

    Update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	printf("Size of a char:%c byte(s)\n", sizeof(char));
-	printf("Size of an int:%d byte(s)\n", sizeof(int));
-	printf("Size of a long int:%Lf byte(s)\n", sizeof(double));
-	printf("Size of a long long int:%lf byte(s)\n", sizeof(float));
-	printf("Size of a float:%f byte(s)\n", sizeof(float));
+	printf("Size of a char:%u byte(s)\n", sizeof(char));
+	printf("Size of an int:%u byte(s)\n", sizeof(int));
+	printf("Size of a long int:%u byte(s)\n", sizeof(long int));
+	printf("Size of a long long int:%u byte(s)\n", sizeof(long long int));
+	printf("Size of a float:%u byte(s)\n", sizeof(float));
 return (0);
 }
 
