4643207570454a10ade2c47d31b606a260116b37
Yara-ib <yr.ap.23@gmail.com>
2023-08-11 18:36:50 -0700

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char x;
+
+	for (i = 0, i <= 26, i++)
+	{
+		putchar("%c\n", x);
+	}
+	return (0);
+}
+
