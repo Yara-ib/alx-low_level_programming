68cda8a76aa846811837f9b029e3d31d87403989
Yara-ib <yr.ap.23@gmail.com>
2023-08-15 13:32:31 -0700

    Update

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,13 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Return: Always 0 (Success)
  */
+
+void print_alphabet(void):x
+return (0);
 
 int main(void)
 {
