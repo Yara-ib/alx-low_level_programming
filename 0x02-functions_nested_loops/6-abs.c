2b24520fb61841d4a07ad13be7b2cd977b44a9fa
Yara-ib <yr.ap.23@gmail.com>
2023-08-15 16:30:39 -0700

    Update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ * @int: Parameter for _abs()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int)
+{
+	int x;
+
+if (x < 0)
+{
+	_putchar('0' + (x * -1);
+}
+else
+{
+	_putchar('0' + x);
+}
+return (0);
+}
