4445a04fc38688de5e490ff39532cc46813f9fe8
Yara-ib <yr.ap.23@gmail.com>
2023-08-15 15:51:36 -0700

    Update

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ *
+ * @c: Parameter for _isalpha()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
