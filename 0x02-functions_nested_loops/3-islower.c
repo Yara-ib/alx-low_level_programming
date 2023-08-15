534f829a73d6586d4ab3bab92d2063199eae816c
Yara-ib <yr.ap.23@gmail.com>
2023-08-15 15:02:15 -0700

    Update

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * @c: Parameter for _islower()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _islower(int c)
+{
+	char c;
+
+	if (_islower(c))
+	{
+		_putchar('0' + 1);
+	}
+	else
+	{
+		_putchar('0');
+	}
+	return (0);
+}
