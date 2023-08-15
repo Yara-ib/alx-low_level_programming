eaf25c3feb0a706d0765f38ef3bf4b9d2e61d6a2
Yara-ib <yr.ap.23@gmail.com>
2023-08-15 16:08:43 -0700

    Update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,22 +13,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar(43);
-		_putchar(',');
-		_putchar(' ');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar(45);
-		_putchar(',');
-		_putchar(' ');
 		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		return (0);
 	}
 }
