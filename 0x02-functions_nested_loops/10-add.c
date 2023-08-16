9ce766347c60c55a972f672d0ef3d58f9609adfd
Yara-ib <yr.ap.23@gmail.com>
2023-08-16 14:52:05 -0700

    Update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - Entry point
+ *
+ * @x: Parameter for add()
+ *
+ * @i: Parameter for add()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int i, int x)
+{
+	int z = i + x;
+
+	return (z);
+
+}
