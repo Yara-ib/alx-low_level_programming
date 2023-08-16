4d1d4c1b900aee52e55bfb4cb41dc53f49bdbb44
Yara-ib <yr.ap.23@gmail.com>
2023-08-15 17:23:55 -0700

    Update

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @x: Parameter for print_last_digit()
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int x)
+{
+	int z = x % 10;
+
+	return (z);
+
+}
