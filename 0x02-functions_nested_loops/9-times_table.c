dcedbc0baf5e832fe7c0ac6a48819761279df23e
Yara-ib <yr.ap.23@gmail.com>
2023-08-16 09:15:43 -0700

    Update

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,17 +8,28 @@
 
 void times_table(void)
 {
-	int i, x;
+	int i, x, n;
 
 	for (i = 0; i < 10; ++i)
 	{
 		for (x = 0; x < 10; ++x)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
-			_putchar('\n');
+			n = i * x;
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+				_putchar(n % 10 + '0');
+			}
+			else
+			{
+				_putchar(n + '0');
+			}
+			if (x < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
-
+		_putchar('\n');
 	}
 }
