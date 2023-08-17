2e02c3a5c7572290218ebb6c38045dd07d3a042c
Yara-ib <yr.ap.23@gmail.com>
2023-08-17 08:19:04 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Entry point
+ *
+ * @n: Parameter for print_diagonal()
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; ++i)
+	{
+		if (n > 0)
+		{
+			_putchar(92);
+		}
+	}
+	_putchar('\n');
+}
