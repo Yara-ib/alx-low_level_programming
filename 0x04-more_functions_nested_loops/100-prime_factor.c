418035bf916acd6577dd1832c085e290ffa308f6
Yara-ib <yr.ap.23@gmail.com>
2023-08-18 14:34:18 -0700

    Update

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, n, l;
+	l = n % x;
+
+	for (x = 2; x <= n; x++)
+	{
+		if (l == 0)
+		{
+			printf("%d", l);
+			for 
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (x == 100)
+		{
+			printf("Buzz");
+		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", x);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
