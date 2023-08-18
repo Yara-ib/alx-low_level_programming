af98d426b2f59026c053e12a9714b9f3eee72fb3
Yara-ib <yr.ap.23@gmail.com>
2023-08-17 18:00:46 -0700

    Update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,25 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if ((x % 15) == 0)
+		if (x % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if ((x % 3) == 0)
+		else if (x % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if ((x % 5) == 0)
+		else if (x % 5 == 0)
 		{
-			printf("Buzz ");
+			if (x == 100)
+			{
+				printf("Buzz");
+			}
+			else
+			{
+				printf("Buzz ");
+			}
+		}
 		}
 		else
 		{
