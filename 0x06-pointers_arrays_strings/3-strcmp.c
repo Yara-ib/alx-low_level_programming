637c548d76f2cadc3930b1f8e96aab80807f6bd0
Yara-ib <yr.ap.23@gmail.com>
2023-08-24 08:42:47 +0300

    Update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,17 @@
 int _strcmp(char *s1, char *s2)
 {
 	int x;
+	int i = 0;
 
 	for (x = 0; s1[x] != 0; ++x)
 	{
-		if (s1 > s2)
+		if ((s1[x] - s2[i]) > 0)
 		{
-			return (15);
+			return ((s1[x] - s2[i]));
 		}
-		else if (s1 < s2)
+		else if ((s1[x] - s2[i]) < 0)
 		{
-			return (-15);
+			return ((s1[x] - s2[i]));
 		}
 		else
 		{
