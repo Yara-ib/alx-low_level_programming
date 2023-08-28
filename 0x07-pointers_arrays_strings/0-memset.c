b898a71d1bcabe41809baaf38dea3736bcfbeddb
Yara-ib <yr.ap.23@gmail.com>
2023-08-28 22:45:24 +0300

    Update

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,12 +13,11 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned i;
+	unsigned int i;
 
-for (i = 0; i < n; ++i)
+for (i = 0; i < n; i++)
 {
-	*s = b;
-
+	s[i] = b;
 }
 return (s);
 }
