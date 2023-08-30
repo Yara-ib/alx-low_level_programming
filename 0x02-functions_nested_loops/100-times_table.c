fe5878a15fbf4af83efed6cdbe927144ca0e62dd
Yara-ib <yr.ap.23@gmail.com>
2023-08-30 05:25:53 +0300

    Update

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,26 +1,44 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_times_table - Entry point
  * @n: Parameter for print_times_table()
  */
+
 void print_times_table(int n)
 {
-	int i, j;
+	int i, x, m;
 
-  if (n < 0 || n > 15) {
-    return;
-  }
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
 
-  for (i = 0; i <= n; i++) {
-    for (j = 0; j <= n; j++) {
-      if (j == 0) {
-        _putchar('0');
-      } else {
-        _putchar(',');
-        _putchar(' ');
-        _putchar(i * j + '0');
-      }
-    }
-    _putchar('\n');
-  }
+			for (x = 1; x <= n; x++)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				m = i * x;
+
+				if (m <= 99)
+					_putchar(' ');
+				if (m <= 9)
+					_putchar(' ');
+
+				if (m >= 100)
+				{
+					_putchar((m / 100) + '0');
+					_putchar(((m / 10)) % 10 + '0');
+				}
+				else if (m <= 99 && m >= 10)
+				{
+					_putchar((m / 10) + '0');
+				}
+				_putchar((m % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
 }
