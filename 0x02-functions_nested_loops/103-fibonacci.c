6fd34899fecfc2dd969edf98701b14559f91bf8f
Yara-ib <yr.ap.23@gmail.com>
2023-08-30 05:47:06 +0300

    Update

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always (0) (success)
+ */
+
+int main(void)
+{
+	int limit = 4000000;
+	int prev = 1;
+	int curr = 2;
+	int sum = 0;
+
+	while (curr <= limit)
+	{
+		if (curr % 2 == 0)
+		{
+			sum += curr;
+		}
+
+		int next = prev + curr;
+
+		prev = curr;
+		curr = next;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
