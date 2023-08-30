2b3eacfd40d4963930e7d96b0ba18ebc905f1974
Yara-ib <yr.ap.23@gmail.com>
2023-08-30 05:38:42 +0300

    Update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,36 +1,47 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - Entry point
+ * print_fib - Another Entry - another Function
+ * @n: Parameter for print_fib
  * Return: Always (0) (success)
  */
 
-int main(void)
+void print_fib(int n)
 {
-	int i, fib_sequence;
+	int i, fib;
 
 	if (n <= 0)
 	{
 		return;
 	}
 
-	fib_sequence[50];
-	fib_sequence[0] = 1;
-	fib_sequence[1] = 2;
+	fib[50];
+	fib[0] = 1;
+	fib[1] = 2;
 
 	for (i = 2; i < n; i++)
 	{
-		fib_sequence[i] = fib_sequence[i - 1] + fib_sequence[i - 2];
+		fib[i] = fib[i - 1] + fib[i - 2];
 	}
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", fib_sequence[i]);
+		printf("%d", fib[i]);
 		if (i != n - 1)
 		{
 			printf(", ");
 		}
 	}
 	printf("\n");
-return (0);
+}
+
+/**
+ * main - Entry point
+ * Return: Always (0) (success)
+ */
+
+int main(void)
+{
+	print_fib(50);
+	return (0);
 }
