0c85cd026a8ff5b9e3091acd34384cc858354e04
Yara-ib <yr.ap.23@gmail.com>
2023-09-01 05:08:08 +0300

    Update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int _sqrt_helper(int n, int result);
+
 /**
 * _sqrt_recursion - Returns the factorial of a given number.
 * @n: int as a parameter for the function.
@@ -8,20 +10,25 @@
 
 int _sqrt_recursion(int n)
 {
-	if (y == 0)
+	return (_sqrt_helper(n, 0));
+}
+
+/**
+* _sqrt_helper - Returns n.
+* @n: int as a parameter for the function.
+* @result: int as a parameter for the function.
+* Return: n.
+*/
+
+int _sqrt_helper(int result, int n)
+{
+	if (n * n == result)
 	{
-		return (1);
+		return (n);
 	}
-	else if (x == 0)
-	{
-		return (0);
-	}
-	else if (y < 0)
+	else if (n * n > result)
 	{
 		return (-1);
 	}
-	else
-	{
-		return (x * _pow_recursion(x, y - 1));
-	}
+	return (_sqrt_helper(result, n + 1));
 }
