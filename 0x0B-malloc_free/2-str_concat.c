7d0c4f3b2bebccf6a22891e93a9dfb133b514991
Yara-ib <yr.ap.23@gmail.com>
2023-09-06 00:09:57 +0300

    Update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,6 +16,12 @@ char *str_concat(char *s1, char *s2)
 	sum = 0;
 
 	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
