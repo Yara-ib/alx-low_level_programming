85e1fa84a285cd634acdab9a58500fbbe98aaa19
Yara-ib <yr.ap.23@gmail.com>
2023-09-07 23:06:58 +0300

    Update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,19 +14,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	int *s;
 	unsigned int i;
 
-
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 
 	s = malloc(sizeof(size) * (nmemb + 1));
 
 	if (s == NULL)
 		return (NULL);
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
 	for (i = 0; i < nmemb; i++)
 		s[i] = 0;
-
 	s[i] = '\0';
 	return (s);
 }
