11ae1e756a747db59f650b3358f8d97f6de9d355
Yara-ib <yr.ap.23@gmail.com>
2023-09-08 04:43:37 +0300

    Update

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,10 +16,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	n = ptr;
 
-	if (new_size <= old_size)
+	if (new_size == old_size)
 		return (ptr);
 
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
