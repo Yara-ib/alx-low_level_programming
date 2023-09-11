411f9fac351b09dbf61f5d12bfda256c36b43fe8
Yara-ib <yr.ap.23@gmail.com>
2023-09-11 12:32:46 +0300

    Update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,8 +14,6 @@ void print_dog(struct dog *d)
 	{
 		if (d->name == NULL)
 			printf("Name: (nil)");
-		if (d->age == 0)
-			printf("Age: (nil)");
 		if (d->owner == NULL)
 			printf("Owner: (nil)");
 		printf("Name: %s\n", d->name);
