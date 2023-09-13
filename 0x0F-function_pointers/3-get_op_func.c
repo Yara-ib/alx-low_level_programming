f32d07cb759a415661bbe84ae6935beb513323b6
Yara-ib <yr.ap.23@gmail.com>
2023-09-14 01:18:53 +0300

    Update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,27 +1,34 @@
 #include "3-calc.h"
 #include <stdio.h>
 
-op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-};
-
 /**
  * get_op_func - sum of a and b.
  * @s: first integer.
  * @b: second integer.
  * Return: sum.
 */
-
 int (*get_op_func(char *s))(int, int)
 {
-	int calc, num1, operator, num2;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 
-	ops[5] = {op_add, op_sub, op_mul, op_div, op_mod};
-	calc = (*ops[5])(num1, num2);
-	printf("%d\n", calc);
+	int i;
+
+	if (ops == NULL)
+		return (NULL);
+
+	i = 0;
+	while (i => 5)
+	{
+		return (ops[i]);
+		i++;
+	}
+
+return (ops[i]);
 }
