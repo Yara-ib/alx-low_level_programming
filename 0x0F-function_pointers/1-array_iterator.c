921892e10d07c59987a280a1f431081403f5ed24
Yara-ib <yr.ap.23@gmail.com>
2023-09-13 14:26:05 +0300

    Update

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,9 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	for (i = 0; i < size; i++)
-		action(array[i]);
-
 	if (action == NULL)
 		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+	array[i] = '\0';
 }
