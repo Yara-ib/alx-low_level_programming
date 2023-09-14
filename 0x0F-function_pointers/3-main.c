0ca42cf2b0f9815699d2ce19d1e9d5ae373b7edf
Yara-ib <yr.ap.23@gmail.com>
2023-09-14 04:38:31 +0300

    Update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -34,7 +34,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (100);
 	}
-
-	printf("%d\n", get_op_func(op)(num1, num2));
+	printf("%d\n",get_op_func(argv[2])(num1, num2));
 	return (0);
 }
