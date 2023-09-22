44fd37baccb2605ab04b278b0a2bec48a02c0c5e
Yara-ib <yr.ap.23@gmail.com>
2023-09-23 02:12:55 +0300

    asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,17 @@
 section .data
-	hello db "Hello, Holberton", 0
-	format dq "%s", 0
+	hello db "Hello, Holberton", 10, 0
+	format db "%s", 0
 
 section .text
 	extern printf
-	extern exit
-
 	global main
 
 main:
 	push rbp
 	mov rdi, format
-	mov rsi, hello
+	lea rsi, [hello]
 	xor eax, eax
 	call printf
 	pop rbp
-
-	xor edi, edi
-	mov eax, 60
-	syscall
+	xor eax, eax
+	ret
