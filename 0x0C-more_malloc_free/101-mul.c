9e13103b34e27d19584316220e8a1980bf52aeb7
Yara-ib <yr.ap.23@gmail.com>
2023-09-24 04:18:41 +0300

    mul-1

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,71 +4,63 @@
 #include <string.h>
 
 /**
- * _mul - reallocates a memory block using malloc and free.
- * @mul: pointer to the memory previously allocated.
- * @num1: size, in bytes, of the allocated space for ptr.
- * @num2: new size, in bytes of the new memory block.
- * Return: pointer to the newly allocated space.
+ * _isdigit - checks if it's digit or not.
+ * @num: Parameter for _isdigit()
+ * Return: Always 0 (Success)
+ */
+
+int _isdigit(long num)
+{
+	if (num >= '0' && num <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * mul - multiplies two positive numbers.
+ * @num1: Parameter for mul()
+ * @num2: Parameter for mul()
+ * Return: Always 0 (Success)
+ */
+
+int mul(long num1, long num2)
+{
+	return (num1 * num2);
+}
+
+/**
+* main - Entry point.
+* @argc: Count for arguments.
+* @argv: Vector to arguments.
+* Return: Always (0); success.
 */
-
-int isNumber(char *str)
-{
-	long unsigned int i;
-
-	for (i = 0; i < strlen(str); i++)
-	{
-		if (str[i] < '0' || str[i] > '9')
-			return 0;
-	}
-	return 1;
-}
-
-char* multiply(char *num1, char *num2)
-{
-	int i, j, mul, sum, len, len2, len1;
-	char *res, *result;
-
-	len1 = strlen(num1);
-	len2 = strlen(num2);
-	len = len1 + len2;
-	result = calloc(len, sizeof(int));
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		for (j = len2 - 1; j >= 0; j--)
-		{
-			mul = (num1[i] - '0') * (num2[j] - '0');
-			sum = result[i + j + 1] + mul;
-			result[i + j] += sum / 10;
-			result[i + j + 1] = sum % 10;
-		}
-	}
-
-	while (len > 0 && result[len - 1] == 0)
-		len--;
-
-	res = malloc((len + 1) * sizeof(char));
-	for (i = 0; i < len; i++)
-		res[i] = result[i] + '0';
-	res[len] = '\0';
-	free(result);
-	return res;
-}
 
 int main(int argc, char *argv[])
 {
-	char *num1, *num2, *result;
+	long num1, num2, i, j;
 
-	if (argc != 3 || !isNumber(argv[1]) || !isNumber(argv[2]))
+	num1 = atol(argv[1]);
+	num2 = atol(argv[2]);
+
+	if (argc != 3)
 	{
 		printf("Error\n");
-		return 98;
+		return (98);
 	}
 
-	num1 = argv[1];
-	num2 = argv[2];
-	result = multiply(num1, num2);
-	printf("%s\n", result);
-	free(result);
-	return 0;
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!_isdigit((long)(argv[i][j])))
+			{
+				printf("Error\n");
+				return (98);
+			}
+		}
+	}
+
+	printf("%d\n", mul(num1, num2));
+	return (0);
 }
