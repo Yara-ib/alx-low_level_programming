6f5576cda7c2712d3a07840ada8f58e2719d3ebe
Yara-ib <yr.ap.23@gmail.com>
2023-09-25 22:59:46 +0300

    more free

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list & set head to NULL
+ * @head: pointer to the head of the node.
+ * Return: nothing, freed memory.
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+}
