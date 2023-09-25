8ede2afd7fd2253cb1f774b3c421c0879cd57608
Yara-ib <yr.ap.23@gmail.com>
2023-09-25 23:28:16 +0300

    double free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,12 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *ptr;
 
+	if (head == NULL)
+		return;
+
 	while (*head)
 	{
 		ptr = *head;
 		*head = (*head)->next;
 		free(ptr);
 	}
-	if (head == NULL)
-		return;
 }
