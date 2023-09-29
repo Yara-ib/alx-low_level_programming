b8039f7fff2cde7e8b58be2d056b18c7cb2cb78b
Yara-ib <yr.ap.23@gmail.com>
2023-09-29 05:33:56 +0300

    flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,9 +9,14 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int flipped = n ^ m, num = 0;
+	unsigned long int diff = n ^ m;
+	unsigned int count = 0;
 
-	for (; flipped > 0; num++)
-		flipped &= (flipped - 1);
-	return (num);
+	while (diff > 0)
+	{
+		count += diff & 1;
+		diff >>= 1;
+	}
+
+	return (count);
 }
