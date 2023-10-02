715f4c179498f73def13c303974ccad2f5e64444
Yara-ib <yr.ap.23@gmail.com>
2023-10-03 01:30:44 +0300

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,13 +18,12 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		creat(filename, S_IRUSR | S_IWUSR);
-	else
+	if (text_content != NULL)
+	{
 		w_letters = write(fd, text_content, strlen(text_content));
 			if (w_letters <= 0)
 				return (-1);
-
+	}
 	close(fd);
 	return (1);
 }
