cc0e3a6ab66c69d7fccb7afd90f397911c8dd341
Yara-ib <yr.ap.23@gmail.com>
2023-10-03 05:04:26 +0300

    copyurself

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,30 +2,15 @@
 #define BUFFER_SIZE 1024
 
 /**
- * print_error - point.
- * @message: arguments count passed on to the program.
- * @code: vector to arguments passed and saved in a NULL terminated array.
- * @filename:or to arguments passed and saved in a NULL term
- * @fd: or to arguments passed and saved in a NULL term
- * Return: 0: Na-da.
-*/
-
-void print_error(int code, char *message, char *filename, int fd)
-{
-	dprintf(2, message, filename, fd);
-	exit(code);
-}
-
-/**
  * main - Entry point.
  * @argc: arguments count passed on to the program.
  * @argv: vector to arguments passed and saved in a NULL terminated array.
  * Return: 0: Success.
 */
+
 int main(int argc, char *argv[])
 {
-	int source_fd, dest_fd;
-	ssize_t bytes_read, bytes_written;
+	ssize_t readable, writable, fd_from, fd_to;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
@@ -33,31 +18,37 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	source_fd = open(argv[1], O_RDONLY);
-	if (source_fd == -1)
-		print_error(98, "Error: Can't read from file %s\n", argv[1], source_fd);
-
-	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
-					S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (dest_fd == -1)
-		print_error(99, "Error: Can't write to %s\n", argv[2], dest_fd);
-
-	while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0)
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
 	{
-		bytes_written = write(dest_fd, buffer, bytes_read);
-		if (bytes_written <= 0)
-			print_error(99, "Error: Can't write to %s\n", argv[2], dest_fd);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-
-	if (bytes_read == -1)
-		print_error(98, "Error: Can't read from file %s\n", argv[1], source_fd);
-
-	if (close(source_fd) == -1 || close(dest_fd) == -1)
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
+	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n",
-					(close(source_fd) == -1) ? source_fd : dest_fd);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	readable = read(fd_from, buffer, BUFFER_SIZE);
+	if (readable <= 0)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	writable = write(fd_to, buffer, readable);
+	if (writable <= 0)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	if (close(fd_from) < 0 || close(fd_to) < 0)
+	{
+		dprintf(2, "Error: Can't close fd %ld\n",
+			(close(fd_from) == -1) ? fd_from : fd_to);
 		exit(100);
 	}
+	close(fd_from);
+	close(fd_to);
 	return (0);
 }
