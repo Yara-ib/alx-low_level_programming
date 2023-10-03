cf0f14f5cc2f1d09e43d7ed60661dc402845c3b2
Yara-ib <yr.ap.23@gmail.com>
2023-10-03 05:32:04 +0300

    copyurself

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,14 +18,12 @@ int main(int argc, char *argv[])
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
 	if (fd_to == -1)
 	{
@@ -33,8 +31,7 @@ int main(int argc, char *argv[])
 		close(fd_from);
 		exit(99);
 	}
-
-	readable = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((readable = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		writable = write(fd_to, buffer, readable);
 		if (writable <= 0)
@@ -45,11 +42,11 @@ int main(int argc, char *argv[])
 			exit(99);
 		}
 	}
-
 	if (readable == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
+		close(fd_to);
 		exit(98);
 	}
 	return (0);
