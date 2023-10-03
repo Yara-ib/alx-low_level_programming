3158504067be400b32a9417af33ac0b3265be8de
Yara-ib <yr.ap.23@gmail.com>
2023-10-03 05:13:50 +0300

    copyurself

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,8 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	ssize_t readable, writable;
+	ssize_t readable, writable, fd_from, fd_to;
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
@@ -25,33 +24,30 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 	readable = read(fd_from, buffer, BUFFER_SIZE);
-	if (readable > 0)
-	{
-		writable = write(fd_to, buffer, readable);
-		if (writable != readable)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
 	if (readable == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
+	writable = write(fd_to, buffer, readable);
+	if (writable != readable)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", (close(fd_from) == -1) ? fd_from : fd_to);
+		dprintf(2, "Error: Can't close fd %d\n",
+		(close(fd_from) == -1) ? fd_from : fd_to);
 		exit(100);
 	}
-	close(fd_from);
-	close(fd_to);
 	return (0);
 }
