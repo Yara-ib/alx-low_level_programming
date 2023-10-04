1db6b614b7837aceb2e4a4176269006c9c51b8e2
Yara-ib <yr.ap.23@gmail.com>
2023-10-04 03:33:40 +0300

    another try

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 664);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
