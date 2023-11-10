c81ad4280407a8f2898d1b55c11aa21ad2974f43
Yara-ib <yr.ap.23@gmail.com>
2023-11-10 04:11:59 +0200

    long

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,7 +26,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		(*h) = new_node;
 		return (new_node);
 	}
-	ptr = (*h);
+	else
+		ptr = (*h);
 
 	while (ptr != NULL && idx != 0)
 	{
