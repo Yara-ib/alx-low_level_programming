75a1151142ab00179c3e77d51c92750a97a6ad30
Yara-ib <yr.ap.23@gmail.com>
2023-11-10 04:11:27 +0200

    long

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -12,9 +12,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *new_node, *ptr, *next_ptr = NULL;
 
-	if (h == NULL)
-		return (NULL);
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
@@ -23,28 +20,33 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new_node->prev = NULL;
 	new_node->next = NULL;
 
+	/* cheking if head exists! if not, new node will be the head */
 	if (*h == NULL)
 	{
-		*h = new_node;
+		(*h) = new_node;
 		return (new_node);
 	}
+	ptr = (*h);
 
-	ptr = *h;
-
-	while (idx > 0 && ptr->next != NULL)
+	while (ptr != NULL && idx != 0)
 	{
 		ptr = ptr->next;
 		idx--;
 	}
 
-	next_ptr = ptr->next;
-
-	if (next_ptr != NULL)
+	/* checking if the 1st ptr points to the end of the list or not */
+	if (ptr == NULL)
+	{
+		new_node->prev = ptr;
+		ptr->next = new_node;
+	}
+	else
+	{
+		next_ptr = ptr->next;
+		ptr->next = new_node;
 		next_ptr->prev = new_node;
-
-	new_node->next = next_ptr;
-	new_node->prev = ptr;
-	ptr->next = new_node;
-
+		new_node->prev = ptr;
+		new_node->next = next_ptr;
+	}
 	return (new_node);
 }
