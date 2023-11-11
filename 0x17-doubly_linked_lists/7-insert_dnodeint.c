aa5c1ea534cbcd1d9411928c4d717ea302e111e7
Yara-ib <yr.ap.23@gmail.com>
2023-11-11 03:22:32 +0200

    try

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -9,44 +9,44 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new, *curr;
-	unsigned int b = 0;
+	dlistint_t *new_node, *ptr, *next_ptr = NULL;
 
-
-	new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-	{
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
 		return (NULL);
-	}
-	new->n = n;
-	new->next = NULL;
-	new->prev = NULL;
 
-	if (idx == 0)
-	{
-		new->next = *h;
-		if (*h != NULL)
-		{
-			(*h)->prev = new;
-		}
-		*h = new;
-		return (new);
-	}
-	curr = *h;
-	for (b = 0; b < (idx - 1) && curr != NULL; b++)
-		curr = curr->next;
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
-	if (curr == NULL)
+	if (*h == NULL)
 	{
+		(*h) = new_node;
+		return (new_node);
+	}
+	ptr = (*h);
+
+	while (ptr != NULL && idx > 0)
+	{
+		ptr = ptr->next;
+		idx--;
+	}
+
+	if (idx > 0)
 		return (NULL);
-	}
-	new->next = curr->next;
-	new->prev = curr;
-	if (curr->next != NULL)
+
+	if (ptr == NULL)
 	{
-		curr->next->prev = new;
+		new_node->prev = ptr;
+		ptr->next = new_node;
 	}
-	curr->next = new;
-	return (new);
+	else
+	{
+		next_ptr = ptr->next;
+		ptr->next = new_node;
+		next_ptr->prev = new_node;
+		new_node->prev = ptr;
+		new_node->next = next_ptr;
+	}
+	return (new_node);
 }
