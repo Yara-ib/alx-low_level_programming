c7506c695fedbc22ef95606cff00262ddb8f1ea0
Yara-ib <yr.ap.23@gmail.com>
2023-11-14 03:33:58 +0200

    wtever

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sleep 98
-export LD_PRELOAD=./gm 9 8 10 24 75 9:$LD_LIBRARY_PATH
+LD_PRELOAD=$PWD/../win.so ./gm 9 8 10 24 75 9
+sleep 98 & unset LD_PRELOAD
--- 0x18-dynamic_libraries/win.c
+++ 0x18-dynamic_libraries/win.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-int print(__attribute__((unused))char *format, ...)
+int dprintf(__attribute__((unused))int fd, __attribute__((unused))const char *format, ...)
 {
-	write(1, "9 8 10 24 75 - 9\n", 17);
-	write(1, "Congratulations, you win the Jackpot!\n", 38);
-	exit(EXIT_SUCCESS);
+	sleep(98);
+	return (1);
 }
