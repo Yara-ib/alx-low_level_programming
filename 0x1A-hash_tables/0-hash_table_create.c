95484f50437df2e6424a4177b40002794e39248a
Yara-ib <yr.ap.23@gmail.com>
2023-12-02 03:30:59 +0200

    Hash*Hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,16 +12,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	/* Assigning memory for the Hash Table (overall) */
 	h_table = malloc(sizeof(hash_table_t));
-	if (!h_table)
-	{
+	if (h_table == NULL)
 		return (NULL);
-	}
 
 	/* Assigning values of hash_table_t's members */
-	h_table->size = size;
 	/* Array; double ptr points to the array & each node to be created.*/
-	h_table->array = malloc(size * sizeof(hash_node_t));
-	if (!h_table->array)
+	h_table->size = size;
+	h_table->array = malloc(size * sizeof(hash_node_t *));
+	if (h_table->array == NULL)
 	{
 		free(h_table->array);
 		return (NULL);
@@ -29,8 +27,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	/* Assigning values for @each of the array indices */
 	for (; i < size; i++)
-	{
 		h_table->array[i] = NULL;
-	}
 	return (h_table);
 }
