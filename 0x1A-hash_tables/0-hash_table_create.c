fc320c350db7e3a20c7c3907b2ae601cf92b1d76
Yara-ib <yr.ap.23@gmail.com>
2023-12-02 03:16:31 +0200

    Hash*Hash

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,6 +10,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *h_table = NULL;
 	unsigned long int i = 0;
 
+	if (size == 0)
+		return (NULL);
+
 	/* Assigning memory for the Hash Table (overall) */
 	h_table = malloc(sizeof(hash_table_t));
 	if (h_table == NULL)
